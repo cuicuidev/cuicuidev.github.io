20d52eaf646e940729cd7da71afece9a8a49f950
unknown <eduanginho@gmail.com>
2023-08-31 01:24:32 +0200

    estructura básica de archivos, web y componentes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { Socials  } from "./components/Socials";
+import { Projects } from "./components/Projects";
+import { Testimonials } from "./components/Testimonials";
+
+import "./components/Projects";
+import "./components/Testimonials";
