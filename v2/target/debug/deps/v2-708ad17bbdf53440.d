6d2162c29d4429263a5509d5e4dcbcbe9dc067c6
Dmitry Ryzhenkov <dmitryryzhenkov@cuicui.dev>
2023-12-31 19:36:54 +0100

    removed target folder

--- v2/target/debug/deps/v2-708ad17bbdf53440.d
+++
@@ -1,5 +0,0 @@
-/home/cuicuidev/projects/cuicuidev.github.io/v2/target/debug/deps/v2-708ad17bbdf53440: src/main.rs
-
-/home/cuicuidev/projects/cuicuidev.github.io/v2/target/debug/deps/v2-708ad17bbdf53440.d: src/main.rs
-
-src/main.rs:
--- v2/target/debug/v2.d
+++
@@ -1,1 +0,0 @@
-/home/cuicuidev/projects/cuicuidev.github.io/v2/target/debug/v2: /home/cuicuidev/projects/cuicuidev.github.io/v2/src/main.rs
